2fb67c63de2df725029b14ea46589eecb521326a
NooRZe <109274539+NooRZe@users.noreply.github.com>
2025-08-14 11:35:13 +0300

    мап категорий с выбором по индексу

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -9,7 +9,7 @@ function PizzaBlock({ title, price }) {
     <div className="pizza-block">
       <img
         className="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+        src="img/pizzas/cheeseburger.png"
         alt="Pizza"
       />
       <h4 className="pizza-block__title">{title}</h4>
